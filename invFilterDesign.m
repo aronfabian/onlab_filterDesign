70f634f09e5a52505cfc7ecd22251aa2c60a8e7a
aronfabian <aron.sajt@gmail.com>
2017-12-04 19:50:18 +0100

    filterType fix

--- invFilterDesign.m
+++ invFilterDesign.m
@@ -1,19 +1,19 @@
-clear all;
-close all;
+close all
+clear all
 
 START_FREQ = 100; % vizsgálati freki tartomány alja
 END_FREQ = 14000; % vizsgálati freki tartomány teteje
 COMP_FLINES = 1000; % az átviteli fgv számításakor használt felbontás
 PLOT_FLINES = 1000; % a megjelenítéshez használt felbontás
 
-A_FILT = 0; % compensation with A-filter
+A_FILT = 1; % compensation with A-filter
 C_FILT = 0; % compensation with C-filter
 NO_FILT = 1; % no compensation
 
 HPF_BUTTER = 1; % use highpass butterworth filter 
 LPF_BUTTER = 1; % use lowpass butterworth filter
 
-PAUSE = 1; % 0=run without pausing, 1 = run with pausing
+PAUSE = 0; % 0=run without pausing, 1 = run with pausing
 
 fileNames = {
 % 'CutOf_iPhone_5S_MFRecorder_2017-04-27 17-08-07_SweepOnly_h_bSpectr'
@@ -25,11 +25,11 @@ fileNames = {
 main(START_FREQ,END_FREQ,COMP_FLINES,PLOT_FLINES,A_FILT,C_FILT,NO_FILT,HPF_BUTTER,LPF_BUTTER,PAUSE,fileNames);
 
 function main(START_FREQ,END_FREQ,COMP_FLINES,PLOT_FLINES,A_FILT,C_FILT,NO_FILT,HPF_BUTTER,LPF_BUTTER,PAUSE,fileNames)
-    
+
 
     f_interp = logspace(log10(START_FREQ),log10(END_FREQ),COMP_FLINES);
     f_interp_plot = logspace(log10(START_FREQ),log10(END_FREQ),PLOT_FLINES);
-    
+
     if (A_FILT == 1)
         load('A_weight.mat');
         A_interp = interp1(A_weight(:,1), A_weight(:,2) , f_interp, 'pchip');
@@ -41,7 +41,7 @@ function main(START_FREQ,END_FREQ,COMP_FLINES,PLOT_FLINES,A_FILT,C_FILT,NO_FILT,
     load('tolerance.mat');
     tol_interp = interp1(tolerance(:,1), tolerance(:,2:3) , f_interp, 'pchip');
     tol_interp_plot = interp1(tolerance(:,1), tolerance(:,2:3) , f_interp_plot, 'pchip');
-    
+
     fileID = fopen('output.txt','w');
     fprintf(fileID,'Results:\n');
     if (isempty(fileNames))
@@ -58,7 +58,7 @@ function main(START_FREQ,END_FREQ,COMP_FLINES,PLOT_FLINES,A_FILT,C_FILT,NO_FILT,
         % filterType = 2 --> calc with C_Filter
         % filterType = 3 --> calc with NO_Filter
         filterType = 1;
-        while (filterType < 3)
+        while (filterType < 4)
             if ((filterType == 1) && (A_FILT == 0))
                 filterType = 2;
             end
@@ -69,6 +69,16 @@ function main(START_FREQ,END_FREQ,COMP_FLINES,PLOT_FLINES,A_FILT,C_FILT,NO_FILT,
                 break;
             end
             
+            switch filterType
+                case 1
+                    fprintf('A szûrõ\n');
+                case 2
+                    fprintf('C szûrõ\n');
+                case 3
+                fprintf('nincs szûrõ\n');
+            end
+                    
+            
             load([fileNames{fileNum}]);
 
             X_interp = interp1(phone_bFreqs, phone_bX, f_interp, 'pchip');
@@ -76,7 +86,7 @@ function main(START_FREQ,END_FREQ,COMP_FLINES,PLOT_FLINES,A_FILT,C_FILT,NO_FILT,
 
             % H_mic to dB scale
             H_mic = 20*log10(X_interp);
-            
+
             if((filterType == 1) && (A_FILT == 1))
                 % H_mic * A_weight
                 H_mic = H_mic - A_interp;
@@ -129,9 +139,9 @@ function main(START_FREQ,END_FREQ,COMP_FLINES,PLOT_FLINES,A_FILT,C_FILT,NO_FILT,
                     fprintf(fileID,'\t\tButterworth LPF cutoff freq: %0.1f Hz\n',fcb2);
                 end
                 H_mic = H_mic+Hb1+Hb2;
-                
+
             end
-            
+
             H_mic_plot = interp1(f_interp, H_mic, f_interp_plot, 'pchip');
             semilogx(f_interp_plot,H_mic_plot)
             legend(leg(1:5))
@@ -139,13 +149,13 @@ function main(START_FREQ,END_FREQ,COMP_FLINES,PLOT_FLINES,A_FILT,C_FILT,NO_FILT,
             if(PAUSE == 1)
                 pause
             end
-            
+
             filterNum = 0;
             finishFlag = 0;
             while(finishFlag == 0)
                 filterNum = filterNum + 1;
                 finishFlag = 0;
-                
+
                 % fine-tuning of prev. filters
                 if (filterNum > 2)
                     for i = 1:filterNum-1
@@ -193,7 +203,7 @@ function main(START_FREQ,END_FREQ,COMP_FLINES,PLOT_FLINES,A_FILT,C_FILT,NO_FILT,
                 if(max(errorAreas) == 0)
                     fprintf('Class1-es tolerancia sávba tartozó átvitelhez szükséges \nszûrõk száma: ')
                     disp(filterNum-1)
-                    disp('-------------------------')
+                    disp('-------------------------------------------------')
                     figure
                     H_mic_o_plot = interp1(f_interp, H_mic_origin, f_interp_plot, 'pchip');
                     semilogx(f_interp_plot,H_mic_o_plot)
@@ -212,7 +222,7 @@ function main(START_FREQ,END_FREQ,COMP_FLINES,PLOT_FLINES,A_FILT,C_FILT,NO_FILT,
                         [He,~] = parametricEQ(filters(i,1),filters(i,2),filters(i,3),fs,f_interp_plot);
                         H_mic_o_plot = H_mic_o_plot + He; 
                         semilogx(f_interp_plot,He,'DisplayName', string(i))
-                        
+
                         fprintf(fileID,'\t\tParametric Filter %d: gain: %0.2f dB, fc: %0.1f Hz, bandwidth: %0.4f octave\n',i,filters(i,1),filters(i,2),filters(i,3));
 
                     end
@@ -221,7 +231,7 @@ function main(START_FREQ,END_FREQ,COMP_FLINES,PLOT_FLINES,A_FILT,C_FILT,NO_FILT,
                     finishFlag = 1;
 
                 end
-                
+
                 if(finishFlag == 0)
                     % select maximum error area
                     [~, maxAreaNum] = max(errorAreas);
@@ -270,7 +280,8 @@ function main(START_FREQ,END_FREQ,COMP_FLINES,PLOT_FLINES,A_FILT,C_FILT,NO_FILT,
             filterType = filterType + 1;
         end % filterType
     end % fileNum
-    
+
     fclose(fileID);
-    
+
 end
+
