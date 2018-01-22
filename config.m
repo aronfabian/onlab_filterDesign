52d817da3a07bef661cd3bb5e8fe063aa1a3ab80
ferenc009 <marki@hit.bme.hu>
2018-01-22 13:07:10 +0100

    Freq és Gain sorsolás fejlesztés
    - a frekvencia sorsolást módosítottam, hogy ne 40 pont közül sorsoljon, hanem tetszőleges finomsággal
    
    - a gain mivel dB-ben van, ezért nem szorozni kell egy véletlen számmal, hanem +/- -olni. +/-6 dB-re vettem.

--- config.m
+++ config.m
@@ -9,37 +9,37 @@ A_FILT = 1; % compensation with A-filter
 C_FILT = 1; % compensation with C-filter
 NO_FILT = 1; % no compensation
 
-HPF_BUTTER = 0; % use highpass butterworth filter 
-LPF_BUTTER = 0; % use lowpass butterworth filter
+HPF_BUTTER = 1; % use highpass butterworth filter 
+LPF_BUTTER = 1; % use lowpass butterworth filter
 
 PAUSE = 1; % 0=run without pausing, 1 = run with pausing
 
 fileNames = {
- 'CutOf_iPhone_5S_MFRecorder_2017-04-27 17-08-07_SweepOnly_h_bSpectr'
-'CutOf_Samsung Galaxy Ace 20170427_173930_SweepOnly_h_bSpectr'
- 'CutOf_Samsung Galaxy Alpha - 20170427_183001_SweepOnly_h_bSpectr'
+%  'CutOf_iPhone_5S_MFRecorder_2017-04-27 17-08-07_SweepOnly_h_bSpectr'
+% 'CutOf_Samsung Galaxy Ace 20170427_173930_SweepOnly_h_bSpectr'
+%  'CutOf_Samsung Galaxy Alpha - 20170427_183001_SweepOnly_h_bSpectr'
 'CutOf_Samsung Galaxy Mini 2 - 20170427_174623_SweepOnly_h_bSpectr'
 };
 
 %% ParamaetricEQEst
 
-EST_MODE = 2; % 1 = fc,gain,bw; 2 = random parameter select 
+EST_MODE = 1; % 1 = fc,gain,bw; 2 = random parameter select 
 
 % IF EST_MODE = 1
-OUTER_LOOP = 10;
+OUTER_LOOP = 100;
 INNER_LOOP = 20;
 
 % IF EST_MODE = 2
-CYCLES = 500;
+CYCLES = 2000;
 
 %% ToleranceOptim
 
 LIMIT = 0.1; % effective range limit
 MAX_CYCLE = 15; % maxumim number of cycles 
-EQUAL_TOLERANCE = 0.1; % stop if the difference between distance from lower and upper band is under this value
+EQUAL_TOLERANCE = 0.05; % stop if the difference between distance from lower and upper band is under this value
 
 %% ErrorCalc
-ERROR_SEL = 1; % sum of abs (ERROR_SEL = 1)
+ERROR_SEL = 3; % sum of abs (ERROR_SEL = 1)
               % sum of squares (ERROR_SEL = 2)
               % sum of abs where H_mic is outside the tolerance band (ERROR_SEL = 3)
               % sum of squares where H_mic is outside the tolerance band (ERROR_SEL = 4)
